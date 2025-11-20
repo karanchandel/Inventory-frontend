5b59939184d03fe37b1af348a703f10fb04fe5cc
Karan Kumar <chandel.karann@gmail.com>
2025-11-20 11:37:36 +0530

    --kk

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  //imports: [RouterOutlet, RouterLink, RouterLinkActive] -kk,
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('Inventry-frontend');
-}
+} 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/api' // Tu jab backend ready kare, yahan update kar dena
+  apiBaseUrl: 'http://localhost:3000/api' // 
 };
